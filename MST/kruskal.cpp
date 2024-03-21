432493d592f62283c2f3f7ceb46bc36ec1e06511
Franciszek Pietrusiak <fp458987@students.mimuw.edu.pl>
2024-03-21 21:19:20 +0100

    add comments to prim and kruskal

--- MST/kruskal.cpp
+++ MST/kruskal.cpp
@@ -4,16 +4,21 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
+/*
+	Zamiast męczyć się z piseniem long long, first, second, itd
+	możemy utworzyć skrócone nazwy i oszczędzić czas i palce (mniej klepania)
+*/
 using pii = pair<int, int>;
 using ll = long long;
 #define FR first
 #define SD second
-#define PB push_back
+#define PB push_back 
 
 struct node { int p, s; };
 constexpr int MxN = 1e5+5;
 node nodes[MxN];
 
+// DSU
 void init(int v) {
 	nodes[v].p = v;
 	nodes[v].s = 1;
@@ -38,32 +43,37 @@ int main () {
 
 	vector<pair<int, pii>> krawedzie;
 
+	// Wczytujemy krawędzie
 	int n, m; cin >> n >> m;
 	for (int i=1; i<=m; i++) {
 		int a, b, c; cin >> a >> b >> c;
 		krawedzie.PB({c, {a, b}});
 	}
+	
+	// Sortujemy krawędzie po wagach
 	sort(krawedzie.begin(), krawedzie.end());
 
+	// Inicjujemy każdy wierzchołek jako oddzielną spójną
 	for (int i=1; i<=n; i++)
 		init(i);
 
+	// Idziemy po krawędziach i jeśli łączą dwie różne spójne to bierzemy krawędż do MST
 	int krawedzi_w_mst = 0;
-	ll ans = 0;
+	ll suma_wag_mst = 0;
 	for (auto e : krawedzie) {
 		int v = e.SD.FR;
 		int u = e.SD.SD;
 		int w = e.FR;
 		if (find(v) != find(u)) {
 			krawedzi_w_mst++;
-			ans += w;
+			suma_wag_mst += w;
 			join(v, u);
 		}
 	}
 
 	if (krawedzi_w_mst != n-1)
 		cout << "IMPOSSIBLE\n";
-	else cout << ans << "\n";
+	else cout << suma_wag_mst << "\n";
 
 	return 0;
 }
--- MST/prim.cpp
+++ MST/prim.cpp
@@ -15,20 +15,22 @@ bool vis[MxN];
 priority_queue<pii> pq;
 vector<pii> G[MxN];
 
+// Wrzucamy wszystkie krawędzie wychodzące z v do jeszcze nie odwiedzonych wierzchołków do kolejki priorytetowej
 void process(int v) {
     vis[v] = 1;
     for (auto e : G[v]) {
         int u = e.FR;
         int w = e.SD;
         if (!vis[u])
-            pq.push({-w, -u});
+            pq.push({-w, u}); // Kolejka priorytetowa daje podgląd do największego elementu (default'owo), aby mieć dostęp do najmniejszego możemy wrzucać (-1)*element do kolejki, a potem przy wyjmowaniu znowu mnożyć przez (-1)
     }
 }
 
 int main () {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);
-
+	
+	// Wczytujemy graf
     int n, m; cin >> n >> m;
     for (int i=1; i<=m; i++) {
         int a, b, c; cin >> a >> b >> c;
@@ -36,23 +38,23 @@ int main () {
         G[b].PB({a, c});
     }
 
-    process(1);
-    ll mst_suma = 0;
+    process(1); // Wybieramy dowolny wierzchołek w grafie od którego zaczniemy
+    ll suma_wag_mst = 0;
     int krawedzi_w_mst = 0;
     while (!pq.empty()) {
-        int u = -pq.top().SD;
-        int w = -pq.top().FR;
+        int u = pq.top().SD;
+        int w = -pq.top().FR; // Zmieniamy znak po wyjęciu
         pq.pop();
         if (!vis[u]) {
             krawedzi_w_mst++;
-            mst_suma += w;
+            suma_wag_mst += w;
             process(u);
         }
     }
 
     if (krawedzi_w_mst != n-1)
         cout << "IMPOSSIBLE\n";
-    else cout << mst_suma << "\n";
+    else cout << suma_wag_mst << "\n";
 
 	return 0;
 }
