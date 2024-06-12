ac0863aadc8a979a20479172aa8cf476bae3a631
Franciszek Pietrusiak <fp458987@students.mimuw.edu.pl>
2024-06-12 22:31:41 +0200

    add testowanie

---
+++ TESTOWANIE/run.sh
@@ -0,0 +1,20 @@
+i=0
+while true
+do
+	./gen $i > in1
+	./brut < in1 > out1
+	./wzo < in1 > out2
+	if diff out1 out2 > res; then
+		echo "OK $i"
+	else
+		echo "WA $i"
+		echo "Wejscie:"
+		cat in1
+		echo "Brut:"
+		cat out1
+		echo "Rozw:"
+		cat out2
+		break
+	fi
+	((i++))
+done
---
+++ TESTOWANIE/templatka.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
+#define FR first
+#define SD second
+#define PB push_back
+#define deb(...) logger(#__VA_ARGS__, __VA_ARGS__) // debugging /*
+template <typename T> struct tag:reference_wrapper <T>{ using reference_wrapper <T>::reference_wrapper; };
+template <typename T1, typename T2> static inline tag <ostream> operator<<(tag <ostream> os, pair<T1, T2> const& p){ return os.get()<<"{"<<p.first<<", "<<p.second<<"}", os;}
+template <typename Other> static inline tag <ostream> operator<<(tag <ostream> os, Other const& o){ os.get()<<o; return os; }
+template <typename T> static inline tag <ostream> operator <<(tag <ostream> os, vector <T> const& v){ os.get()<<"["; for (int i=0; i<v.size(); i++) if (i!=v.size()-1) os.get()<<v[i]<<", "; else os.get()<<v[i]; return os.get()<<"]", os; }
+template <typename T> static inline tag <ostream> operator <<(tag <ostream> os, set <T> const& s){ vector <T> v; for (auto i: s) v.push_back(i); os.get()<<"["; for (int i=0; i<v.size(); i++) if (i!=v.size()-1) os.get()<<v[i]<<", "; else os.get()<<v[i]; return os.get()<<"]", os; }
+template <typename ...Args> void logger(string vars, Args&&... values) { cout<<"[ "<<vars<<" = "; string delim=""; (..., (cout<<delim<<values, delim=", ")); cout <<" ]\n"; }
+/**/
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	return 0;
+}
